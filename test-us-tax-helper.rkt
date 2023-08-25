b4f1f6c8af919a8accb6ec6f027f6321ecfadab3
Tim Hanson <tbh391@gmail.com>
2023-08-25 12:16:19 +0200

    minor updates

--- test-us-tax-helper.rkt
+++ test-us-tax-helper.rkt
@@ -3,19 +3,20 @@
 (require rackunit "us-tax-helper.rkt")
 
 
-; claim we have values for years 2011 - 2019
+; claim we have values for years we care about
 
 (for ([year (in-range 2011 2020)])
-  (begin
-    (check-equal?
-     (real?
-      (dollars-per-euro-by-year year))
-     #t)
-    (check-equal?
-     (real?
-      (euros-per-dollar-by-year year))
-     #t)))
-    
+  (check-equal?
+   (real?
+    (euros-per-dollar-by-year year))
+   #t))
+
+(for ([year (in-range 2016 2023)])
+  (check-equal?
+   (real?
+    (euros-per-dollar-by-year-per-irs year))
+   #t))
+
 
 ; now check one of these values (collected 2020) against one I used in the past
 (define epsilon 0.001)
@@ -23,5 +24,28 @@
 ; from my notes:
 ; 2011 avg exchange rate: 0.7188
 (check-= 0.7188 (euros-per-dollar-by-year 2011) epsilon)
-         
-  
+;(check-= 0.877 (euros-per-dollar-by-year 2020) epsilon)
+
+;(check-= 0.846 (euros-per-dollar-by-year 2021) epsilon)
+
+
+; curious about discrepancies based on source (i have 2 sources for 2016 - 2021)
+
+;(stream->list (stream-map euros-per-dollar-by-year (in-range 2016 2021)))
+;(stream->list (stream-map euros-per-dollar-by-year-per-irs (in-range 2016 2021)))
+
+; cf. https://en.wikipedia.org/wiki/Symmetric_mean_absolute_percentage_error
+
+(define (symmetric-relative-error m1 m2)
+  (let ((numer (abs (- m1 m2)))
+        (denom (+ (abs m1) (abs m2))))
+    (/ numer denom)))
+
+(stream->list
+ (stream-map
+  (lambda (year)
+    (symmetric-relative-error
+     (euros-per-dollar-by-year year)
+     (euros-per-dollar-by-year-per-irs year)))
+  (in-range 2016 2021)))
+     
--- us-tax-helper.rkt
+++ us-tax-helper.rkt
@@ -31,8 +31,24 @@
 
 (define (euros-per-dollar-by-year year)
   (/ 1.0 (dollars-per-euro-by-year year)))
+
+; source: https://www.irs.gov/individuals/international-taxpayers/yearly-average-currency-exchange-rates
+; 2021 .. 2016
+; Euro Zone 	Euro 	0.846 	0.877 	0.893 	0.848 	0.923 	0.940
+(define (euros-per-dollar-by-year-per-irs year)
+  (cond
+    [(equal? year 2016) 0.940]
+    [(equal? year 2017) 0.923]
+    [(equal? year 2018) 0.848]
+    [(equal? year 2019) 0.893]
+    [(equal? year 2020) 0.877]
+    [(equal? year 2021) 0.846]
+    [(equal? year 2022) 0.951]
+        [else #f]))
   
+
 (provide
  dollars-per-euro-by-year
  euros-per-dollar-by-year
+ euros-per-dollar-by-year-per-irs
  )
